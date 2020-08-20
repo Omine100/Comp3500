3832d966bcaa703705f260b591a9786db0aebc83
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-08-19 19:23:44 -0500

    simple.c

--- Projects/comp3500_project1_usingLinuxTerminal/simple.c
+++ Projects/comp3500_project1_usingLinuxTerminal/simple.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Matthew's Average Square Collector");
+    printf("Matthew's Average Sqvauare Collector");
 
-    int value;
-    int values[10];
-
-    for (int i = 0; i <= 10; i++) {
-        printf("\n\tEnter a value: ");
+    int sqrtArray[10];
+    
+    for (int i = 0; i < 10; i++) {
+        int value;
+        printf("\n\tEnter a number: ");
         scanf("%d", &value);
+        array[i] = sqrt(double value) / 10;
         printf("\n\tYou entered: %d", value);
-        values[i] = value;
+        if (i = 9) {
+            int sum;
+            for (int i = 0; i < 10; i++) {
+                sum = sqrtArray[i] + sum;
+            }
+            printf("\n\nAverage of square roots: ");
+            printf(sum);
+        }
     }
-    return 0;
 }
