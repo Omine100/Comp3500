aae29a07eccaa13b63bf69cfa1ebc8d9d09d105e
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-08-19 22:09:24 -0500

    Update.

--- Projects/comp3500_project1_usingLinuxTerminal/simple.c
+++ Projects/comp3500_project1_usingLinuxTerminal/simple.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Matthew's Average Sqvauare Collector");
+    printf("Matthew's Average Square Collector");
 
-    int sqrtArray[10];
+    double sqrtArray[10], sum, average;
     
-    for (int i = 0; i < 10; i++) {
+    int i = 0;
+    while (i < 10) {
         int value;
         printf("\n\tEnter a number: ");
         scanf("%d", &value);
-        array[i] = sqrt(double value) / 10;
-        printf("\n\tYou entered: %d", value);
-        if (i = 9) {
-            int sum;
-            for (int i = 0; i < 10; i++) {
-                sum = sqrtArray[i] + sum;
-            }
-            printf("\n\nAverage of square roots: ");
-            printf(sum);
-        }
+        printf("\tYou entered: %d", value);
+        sqrtArray[i] = sqrt((value));
+        sum = sum + sqrtArray[i];
+        i++;
     }
+
+    average = sum / 10;
+    printf("\nAverage of squares: %f", average);
 }
