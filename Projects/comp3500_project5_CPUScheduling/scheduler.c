4d73d865edadd33ad4ec5a6133ed7d88719fadab
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-11-08 00:46:51 -0600

    FCFS update
    Need to decide whether I make a waiting time and all of that stuff calculator for each one of the policies or for all them together
    
    Also, if I do them one at a time, do I update wait time when their process starts?

--- Projects/comp3500_project5_CPUScheduling/scheduler.c
+++ Projects/comp3500_project5_CPUScheduling/scheduler.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_TIME 10000
+
 typedef unsigned int u_int;
 typedef struct task {
     u_int pid;    
@@ -15,7 +17,28 @@ typedef struct task {
 } task_t;
 
 void fcfs_policy(task_t task_array[], int finish_array[], int count) {
-    printf("Testing FCFS\n");
+    int process, burstCount, timeCount = 0;
+    for (process = 0; process < count; process++) {
+        for (burstCount = 0; burstCount < task_array[process].burst_time; burstCount++) {
+           printf("<time %d> process %d is running\n", timeCount, task_array[process].pid);
+           timeCount = timeCount+1;
+        }
+        printf("<time %d> process %d is finished...\n", timeCount, task_array[process].pid);
+    }
+    printf("<time %d> All processes finish ......\n", timeCount);
+
+    //Creating a waiting time array and calculating values
+    int waitingTime[count];
+    waitingTime[0] = 0;
+    for (int i = 1; i < count; i++) {
+        waitingTime[i] = task_array[i-1].burst_time + waitingTime[i-1];
+    }
+
+    //Creating a turnaround time array and calculating values
+    int turnaroundTime[count];
+    for (int i = 1; i < count; i++) {
+        turnaroundTime[i] = waitingTime[i] + task_array[i].burst_time;
+    }
 }
 
 void rr_policy(task_t task_array[], int finish_array[], int count, int time_quantum) {
