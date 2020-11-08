70db73a4fda42412bf71b9a4ebddc03d787c8f1f
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-11-07 23:54:31 -0600

    Update input.c

--- Projects/comp3500_project5_CPUScheduling/input.c
+++ Projects/comp3500_project5_CPUScheduling/input.c
@@ -13,6 +13,7 @@
 #define MAX_TASK_NUM 32
 
 int main(int argc, char *argv[]) {
+    //Variables
     char *file_name;
     char *policy_type;
     int type;
@@ -22,28 +23,33 @@ int main(int argc, char *argv[]) {
     task_t task_array[MAX_TASK_NUM];
     int finish_array[MAX_TASK_NUM];
 
+    //Telling the user how to use the program by commands
     if (argc == 1 || argc > 4) {
         printf("Usage: scheduler task_list_file [FCFS|RR|SRTF] [time_quantum]\n");
         return EXIT_FAILURE;
     }
 
+    //Opening the file, or informing the user about an error with the file
     file_name = argv[1];
     if (!(fp = fopen(file_name, "r"))) {
         printf("File can't be opened. Retry...\n");
         return EXIT_FAILURE;
     }
 
+    //Reading the file
     count = 0;
     while (fscanf(fp, "%u %u %u", &task_array[count].pid, &task_array[count].arrival_time, &task_array[count].burst_time)!= EOF) {
         count++;
     }
 
+    //Reading the policy type, or informing the user about an error with the policy type
     policy_type = argv[2];
     if (strcmp(argv[2], "FCFS") != 0 && strcmp(argv[2], "RR") != 0 && strcmp(argv[2], "SRTF") != 0) {
         printf("Policy type not recognized. Retry...\n");
         return EXIT_FAILURE;
     }
 
+    //Output to the reader prior to scheduling
     printf("Scheduling Policy: %s\n", policy_type);
     printf("There are %u tasks loaded from \"%s\". ", count, file_name);
     printf("Press any key to continue ...");
@@ -51,6 +57,7 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     printf("==================================================================\n");
 
+    //Policy picker leading into scheduling
     if (strcmp(argv[2], "FCFS") == 0) {
         fcfs_policy(task_array, finish_array, count);
     } else if (strcmp(argv[2], "RR") == 0) {
@@ -61,6 +68,7 @@ int main(int argc, char *argv[]) {
     }
     printf("==================================================================\n");
 
+    //Output to the user after scheduling
     displayStat();
     printf("==================================================================\n");
     
